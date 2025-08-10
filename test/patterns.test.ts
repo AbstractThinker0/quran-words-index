43f824c5eac189a8995e6a4878a4e8000156b6d9
AbstractThinker0 <arabicwriter09@gmail.com>
2025-08-10 10:01:49 +0100

    Add انْ prefix extracted roots

--- test/patterns.test.ts
+++ test/patterns.test.ts
@@ -4,7 +4,7 @@ import { commonArabicDiacritics } from "../src/consts";
 import { getFirstRoot, getSecondRoot, getThirdRoot } from "../src/rootExtract";
 
 describe("patterns tests", () => {
-  test("Check اسْتَ - اسْتِ - اسْتُ - أَسْتَ prefix extracted roots", () => {
+  test("Check اسْتَ - اسْتِ - اسْتُ - أَسْتَ - انْ prefix extracted roots", () => {
     for (const wordKey in wordsIndex) {
       const currWord = wordsIndex[wordKey];
       const splittedWord = splitArabicLetters(currWord.unprefixed);
@@ -36,6 +36,23 @@ describe("patterns tests", () => {
         expect(currWord.extracted_root).toBe(getFirstRoot(currWord.unprefixed));
       } else if (currWord.bound_prefix === "أَسْتَ") {
         expect(currWord.extracted_root).toBe(getFirstRoot(currWord.unprefixed));
+      } else if (currWord.bound_prefix === "انْ") {
+        if (
+          splittedWord.length > 1 &&
+          splittedWord[1].includes(commonArabicDiacritics.shadda)
+        ) {
+          expect(currWord.extracted_root).toBe(
+            getThirdRoot(currWord.unprefixed)
+          );
+        } else if (splittedWord.length > 1 && splittedWord[2] === "ا") {
+          expect(currWord.extracted_root).toBe(
+            getSecondRoot(currWord.unprefixed)
+          );
+        } else {
+          expect(currWord.extracted_root).toBe(
+            getFirstRoot(currWord.unprefixed)
+          );
+        }
       }
     }
   });
