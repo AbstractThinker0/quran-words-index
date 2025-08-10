c03972697872c17e78198370ba35b9d2ce09ff15
AbstractThinker0 <arabicwriter09@gmail.com>
2025-08-10 19:10:46 +0100

    Add نَسْتَ prefix extracted roots

--- test/patterns.test.ts
+++ test/patterns.test.ts
@@ -4,7 +4,7 @@ import { commonArabicDiacritics } from "../src/consts";
 import { getFirstRoot, getSecondRoot, getThirdRoot } from "../src/rootExtract";
 
 describe("patterns tests", () => {
-  test("Check اسْتَ - اسْتِ - اسْتُ - أَسْتَ - انْ - مُسْتَ - يَسْتَ prefix extracted roots", () => {
+  test("Check اسْتَ - اسْتِ - اسْتُ - أَسْتَ - انْ - مُسْتَ - يَسْتَ - نَسْتَ prefix extracted roots", () => {
     for (const wordKey in wordsIndex) {
       const currWord = wordsIndex[wordKey];
       const splittedWord = splitArabicLetters(currWord.unprefixed);
@@ -79,6 +79,8 @@ describe("patterns tests", () => {
             getFirstRoot(currWord.unprefixed)
           );
         }
+      } else if (currWord.bound_prefix === "نَسْتَ") {
+        expect(currWord.extracted_root).toBe(getFirstRoot(currWord.unprefixed));
       }
     }
   });
